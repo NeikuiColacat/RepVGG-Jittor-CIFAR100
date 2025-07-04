03da274c2cde42dabd76daa437ff5c98159a2fbf
NeikuiColacat <neikui.colacat@gmail.com>
2025-07-05 02:41:15 +0800

    change re.sh

--- data_preprocess/reconstruct_val_set.sh
+++ data_preprocess/reconstruct_val_set.sh
@@ -4,4 +4,4 @@ cd ./imagenet/val
 wget https://raw.githubusercontent.com/soumith/imagenetloader.torch/master/valprep.sh
 chmod +x valprep.sh
 ./valprep.sh 
-rm ./imagenet/val/valprep.sh
+rm valprep.sh
