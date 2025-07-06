restore , fix gradient vanish problem

main_train_jittor.py
@@ -16,7 +16,7 @@ def create_model(config):
         channel_scale_A=config['scale_a'],
         channel_scale_B=config['scale_b'],
         group_conv=config['group_conv'],
-        classify_classes=10,  
+        classify_classes=1000,  
         model_type=config['model_type']
     )
 
@@ -48,36 +48,7 @@ def train_model(config_path, resume_path = None):
 
     optimizer = get_optimizer(model, config)
     scheduler = get_scheduler(optimizer, config)
-    # train_loader, val_loader = get_imagenet_dataloaders(config)
-
-
-    ###########################
-    normalize = jt.transform.ImageNormalize(
-        mean=[0.4914, 0.4822, 0.4465], 
-        std=[0.2023, 0.1994, 0.2010]
-    )
-    
-    train_transform = jt.transform.Compose([
-        jt.transform.Resize((36, 36)),  # 先放大
-        jt.transform.RandomCrop(32),    # 然后随机裁剪到32x32
-        jt.transform.RandomHorizontalFlip(0.5),
-        jt.transform.ToTensor(),
-        normalize
-    ])
-    
-    test_transform = jt.transform.Compose([
-        jt.transform.ToTensor(),
-        normalize
-    ])
-    
-    # 加载数据集
-    train_dataset = CIFAR10(train=True, transform=train_transform, download=True)
-    test_dataset = CIFAR10(train=False, transform=test_transform)
-    
-    train_loader = train_dataset.set_attrs(batch_size=128, shuffle=True,num_workers=16)
-    val_loader = test_dataset.set_attrs(batch_size=256, shuffle=False,num_workers=16)
-
-    ############################
+    train_loader, val_loader = get_imagenet_dataloaders(config)
 
     loss_func = nn.CrossEntropyLoss()
     start_epoch = 0

train/optimizer_jittor.py
@@ -8,18 +8,16 @@ def filter_param(model: nn.Module):
     weight_decay_param = []
     no_decay_param = []
 
-    get_param = lambda module : (
-        [param for param in module.parameters() if param.requires_grad]
-    )
-
     for module in model.modules():
         if isinstance(module, (nn.Conv2d, nn.Linear)):
-            weight_decay_param += get_param(module)
+            weight_decay_param.append(module.weight)
+            if module.bias is not None :
+                no_decay_param.append(module.bias)
         else:
-            no_decay_param += get_param(module)
+            no_decay_param += [p for p in module.parameters(recurse = False) if p.requires_grad]
     
     return [
-        {'params': weight_decay_param},
+        {'params': weight_decay_param , 'weight_decay' : 1e-4},
         {'params': no_decay_param, 'weight_decay': 0}
     ]
 
@@ -29,7 +27,7 @@ def get_optimizer(model, config):
     momentum = config['momentum']
 
     return optim.SGD(
-        model.parameters(),
+        filter_param(model),
         lr=lr,
         momentum=momentum,
         weight_decay=weight_decay,

train/train_jittor.py
@@ -68,7 +68,7 @@ def train_one_epoch(model: nn.Module, train_loader, optimizer, loss_func, epoch_
         
         optimizer.zero_grad()
         output = model(data)
-        loss = nn.cross_entropy_loss(output, target)
+        loss = loss_func(output, target)
 
         optimizer.backward(loss)
         optimizer.step()
@@ -101,7 +101,7 @@ def val_one_epoch(model: nn.Module, val_loader, loss_func):
     process_bar = tqdm(val_loader, desc='validating')
     for data, target in process_bar:
         output = model(data)
-        loss = nn.cross_entropy_loss(output, target)
+        loss = loss_func(output, target)
         
         cur_loss += loss.item()
         