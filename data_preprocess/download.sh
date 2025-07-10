36f0ff6b43b6c5ca767eb385ac0814f7aa244e75
NeikuiColacat <neikui.colacat@gmail.com>
2025-07-11 04:44:02 +0800

    tidy repo

--- data_preprocess/download.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-aria2c -c -x 16 -s 16 \
-  "https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_train.tar"
-
-aria2c -c -x 16 -s 16 \
-  "https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_val.tar"
-
--- data_preprocess/reconstruct_val_set.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd ./imagenet/val
-wget https://raw.githubusercontent.com/soumith/imagenetloader.torch/master/valprep.sh
-chmod +x valprep.sh
-./valprep.sh 
-rm valprep.sh
